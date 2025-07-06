2b277544e293fa079403beca86ee3b92c017294d
{Shimgo2008} <{Shimgo2008@gmail.com}>
2025-07-06 11:52:32 +0900

    Initial Commit

---
+++ FireWorkSimulator/FireWorkSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireWorkSimulatorApp.swift
+//  FireWorkSimulator
+//
+//  Created by 岩澤慎平 on 2025/07/06.
+//
+
+import SwiftUI
+
+@main
+struct FireWorkSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FireWorkSimulatorTests/FireWorkSimulatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  FireWorkSimulatorTests.swift
+//  FireWorkSimulatorTests
+//
+//  Created by 岩澤慎平 on 2025/07/06.
+//
+
+import Testing
+@testable import FireWorkSimulator
+
+struct FireWorkSimulatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
